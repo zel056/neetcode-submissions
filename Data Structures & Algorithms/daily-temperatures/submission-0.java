d172ba3d02b0e7eda84f810a96d282e6b76228b5
zel056 <zel056@ucsd.edu>
2026-09-09 21:35:39 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i =0; i<temperatures.length;i++){
+            while (!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
+                int prev = stack.pop();
+                result[prev] = i-prev;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
