951dd9157e7cef3a8364fc94bd730262fe1fc59d
zel056 <zel056@ucsd.edu>
2026-09-15 11:00:14 -0700

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Double>stack = new Stack<>();
+        int[][] cars = new int[position.length][2];
+
+        for (int i=0; i<position.length; i++){
+            cars[i][0]=position[i];
+            cars[i][1]=speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> Integer.compare(b[0], a[0]));
+
+        for (int i=0;i<position.length;i++){
+            int currPos = cars[i][0];
+            int currSpd = cars[i][1];
+            int diff = target - currPos;
+            double time = (double)diff/currSpd;
+            if (!stack.isEmpty()&&time<=stack.peek()){
+                continue;
+            }
+            stack.push(time);
+        }
+        return stack.size();
+    }
+}
